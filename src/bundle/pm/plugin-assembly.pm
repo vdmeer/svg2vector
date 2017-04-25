2f92f30f3fdc70bcbb986ca2e5c9cddbe264da02
vdmeer <sven@vandermeer.de>
2017-04-25 15:37:59 +0100

    fixed POM problems

--- src/bundle/pm/plugin-assembly.pm
+++ src/bundle/pm/plugin-assembly.pm
@@ -1,6 +1,16 @@
 <plugin>
 	<artifactId>maven-assembly-plugin</artifactId>
 	<version>${pm.version.maven-assembly-plugin}</version>
+	<configuration>
+		<descriptorRefs>
+			<descriptorRef>jar-with-dependencies</descriptorRef>
+		</descriptorRefs>
+		<archive>
+			<manifest>
+				<mainClass>de.vandermeer.svg2vector.Svg2VectorExecs</mainClass>
+			</manifest>
+		</archive>
+	</configuration>
 	<executions>
 		<execution>
 			<id>generate-complete-tar</id>
@@ -14,6 +24,13 @@
 				</descriptors>
 				<finalName>application/${project.artifactId}-${project.version}</finalName>
 			</configuration>
+		</execution>
+		<execution>
+			<id>make-assembly</id>
+			<phase>package</phase>
+			<goals>
+				<goal>single</goal>
+			</goals>
 		</execution>
 	</executions>
 </plugin>
